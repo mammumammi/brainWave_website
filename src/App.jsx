e07f0d28be0cbe84c6f94dd2e1087fd4637ec6b1
Aashin Mohammed A Z <ashinmammu@gmail.com>
2024-12-26 18:42:37 +0530

    12e

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import ButtonGradient from "./assets/svg/ButtonGradient"
 import './App.css'
 import Button from "./components/Button"
 import Header from "./components/Header"
-
+import Hero from "./components/Hero";
 const App = () => {
 
   return (
@@ -10,6 +10,7 @@ const App = () => {
    
     <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
       <Header />
+      <Hero />
       
     </div>
     <ButtonGradient />
